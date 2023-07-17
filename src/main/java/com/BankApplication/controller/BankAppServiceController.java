41728282b06d39f0e4b6eb3636827f89c09dafc5
bkhanal87@gmail.com <bkhanal87@gmail.com>
2023-07-16 20:22:20 -0400

    create an API GET endpoint

--- src/main/java/com/BankApplication/controller/BankAppServiceController.java
+++ src/main/java/com/BankApplication/controller/BankAppServiceController.java
@@ -1,20 +1,24 @@
 package com.BankApplication.controller;
 
+import com.BankApplication.model.User;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/bankuser")
+@RequestMapping("/user")
 public class BankAppServiceController {
-
     // GET Method to retrieve the user
-
-    {
-        public bankUser getbankUserDetails(String id) {
-            return new bankUser("Customer1", "John Doe",
-                    "123 Main Street, Anytown, Oh, 45022", "123-456-789");
+    // add GETMapping here to map to the URL
+    @GetMapping("{id}")
+    // here, we are adding the hard coded values and mapping with the object in the model
+    // this value will be dynamically obtained from the DB later
+    public User getBankUserDetails(String id) {
+        return new User("Customer1",
+                "John Doe",
+                "123 Main Street, Any Town, Oh, 45022",
+                "123-456-789");
     }
-    }
-
 }
